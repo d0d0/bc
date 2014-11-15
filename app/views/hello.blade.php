4ac66f6af054df5548629fcb74181c790ddb4824
D0d0 <jozef.d13@gmail.com>
2014-11-15 23:54:03 +0100

    project init

---
+++ app/views/hello.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link href="ace/style.css" rel="stylesheet" type="text/css">
+		<title>Hello World</title>
+	</head>
+
+	<body>
+		<div id="header">
+			<div id="htext">
+				Editing <b>hello</b></input>
+			</div>
+		</div>
+
+		<div id="editor"></div>
+
+		<script src="js/ace.js" type="text/javascript" charset="utf-8"></script>
+    <script src="js/bcsocket-uncompressed.js"></script>
+		<script src="js/share.uncompressed.js"></script>
+		<script src="js/ace_c.js"></script>
+
+		<script>
+var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/c_cpp");
+
+    sharejs.open('hello', 'text', 'http://62.169.176.249:8000/channel', function(error, doc) {
+	doc.attach_ace(editor);
+});
+		</script>
+	</body>
+</html>	
+
