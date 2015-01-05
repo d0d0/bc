6cf3ef85fed7230f00282043cef95f04754cfe99
D0d0 <jozef.d13@gmail.com>
2015-01-05 09:38:38 +0100

    init

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -2,10 +2,6 @@
 <html>
     <head>
         <link href="css/style.css" rel="stylesheet" type="text/css">
-        <script src="js/ace/ace.js" type="text/javascript" charset="utf-8"></script>
-        <script src="js/bcsocket-uncompressed.js"></script>
-        <script src="js/share.uncompressed.js"></script>
-        <script src="js/ace_c.js"></script>
         <title>Hello World</title>
     </head>
 
@@ -18,9 +14,13 @@
 
         <div id="editor"></div>
 
+        <script src="js/ace/ace.js" type="text/javascript" charset="utf-8"></script>
+        <script src="js/bcsocket-uncompressed.js"></script>
+        <script src="js/share.uncompressed.js"></script>
+        <script src="js/ace_c.js"></script>
         <script>
             var editor = ace.edit("editor");
-            editor.setTheme("ace/theme/monokai");
+            editor.setTheme("ace/theme/twilight");
             editor.getSession().setMode("ace/mode/c_cpp");
 
             sharejs.open('hello', 'text', 'http://62.169.176.249:8000/channel', function (error, doc) {
