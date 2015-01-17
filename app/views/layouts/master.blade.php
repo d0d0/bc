f46c2ea9cbb79e4ae20abcacbd9d8b43656d113f
D0d0 <jozef.d13@gmail.com>
2015-01-17 17:26:58 +0100

    google analytics

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="_token" content="{{ csrf_token() }}">
+        <meta name="google-site-verification" content="kGfEFmvh8xmM3nq7DcZQv5uprQcbqHnKwSNOFbIIvRw" />
         {{ HTML::style('css/bootstrap.min.css') }}
         {{ HTML::style('css/bootstrap-minimit.min.css') }}
         {{ HTML::script('js/jquery.min.js') }}
