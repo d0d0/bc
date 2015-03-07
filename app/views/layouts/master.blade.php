4036a4b5857d154e9cbd66c6a814b525b7d72491
D0d0 <jozef.d13@gmail.com>
2015-03-07 12:36:20 +0100

    shell

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -116,7 +116,7 @@
             @yield('content')
             @endif
         </div>
-        {{ shell_exec('g++ -I/home/pi/gtest-svn/include -L/home/pi/gtest-svn/  /home/pi/gtest-svn/src/gtest_main.cc /home/pi/gtest-svn/samples/sample1.cc /home/pi/gtest-svn/samples/sample1_unittest.cc -lgtest -lpthread -o /home/pi/gtest-svn/samples/main') }}
+        {{ exec('g++ -I/home/pi/gtest-svn/include -L/home/pi/gtest-svn/  /home/pi/gtest-svn/src/gtest_main.cc /home/pi/gtest-svn/samples/sample1.cc /home/pi/gtest-svn/samples/sample1_unittest.cc -lgtest -lpthread -o /home/pi/gtest-svn/samples/main') }}
         {{ shell_exec('/home/pi/gtest-svn/samples/main --gtest_color=yes | sh /home/pi/gtest-svn/samples/ansi2html.sh > /var/www/test.html') }}
         {{ File::get('/var/www/test.html') }}
         .Info o kompilátore
