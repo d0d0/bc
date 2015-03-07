87728a8d941185ee9622f9ab624446be859d032e
D0d0 <jozef.d13@gmail.com>
2015-03-07 13:32:31 +0100

    fix

--- app/controllers/CompilerController.php
+++ app/controllers/CompilerController.php
@@ -5,10 +5,15 @@
  *
  * @author Jozef
  */
-class CompilerController extends BaseController{
+class CompilerController extends BaseController {
 
     public function run() {
-        File::delete('/home/pi/gtest-svn/samples/');
+        if (File::exists('/home/pi/gtest-svn/samples/')) {
+            File::delete('/home/pi/gtest-svn/samples/');
+        }
+        if (File::exists('/var/www/test.html')) {
+            File::delete('/var/www/test.html');
+        }
         File::put('/var/www/test.html', '');
         return View::make('compiler.compiler');
     }
