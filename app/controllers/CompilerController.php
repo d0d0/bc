91cf7a3dd8c9ec4d27136f1be13474505b3580c5
D0d0 <jozef.d13@gmail.com>
2015-03-09 17:46:55 +0100

    migrate to new server

--- app/controllers/CompilerController.php
+++ app/controllers/CompilerController.php
@@ -8,13 +8,13 @@
 class CompilerController extends BaseController {
 
     public function run() {
-        if (File::exists('/home/jduc/gtest-svn/samples/main')) {
-            File::delete('/home/jduc/gtest-svn/samples/main');
+        if (File::exists('/home/jduc/gtest-1.7.0/samples/main')) {
+            File::delete('/home/jduc/gtest-1.7.0/samples/main');
         }
-        if (File::exists(storage_path().'/test.html')) {
-            File::delete(storage_path().'/test.html');
+        if (File::exists(storage_path() . '/test.html')) {
+            File::delete(storage_path() . '/test.html');
         }
-        File::put(storage_path().'/test.html', '');
+        File::put(storage_path() . '/test.html', '');
         return View::make('compiler.compiler');
     }
 
