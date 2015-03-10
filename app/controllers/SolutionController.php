ed36b35549b0161034b68a04d2f0563e7953a715
D0d0 <jozef.d13@gmail.com>
2015-03-10 23:46:20 +0100

    run test

--- app/controllers/SolutionController.php
+++ app/controllers/SolutionController.php
@@ -58,7 +58,7 @@ class SolutionController extends BaseController {
                 File::delete(storage_path() . '/test.html');
             }
             File::put(storage_path() . '/test.html', '');
-            shell_exec('g++ -I/home/jduc/gtest-1.7.0/include -L/home/jduc/gtest-1.7.0/ /home/jduc/gtest-1.7.0/src/gtest_main.cc ' . $includefiles . ' ' . $testfile . ' -lgtest -lpthread -o /home/jduc/gtest-1.7.0/samples/main 2>&1 1>/dev/null');
+            echo shell_exec('g++ -I/home/jduc/gtest-1.7.0/include -L/home/jduc/gtest-1.7.0/ /home/jduc/gtest-1.7.0/src/gtest_main.cc ' . $includefiles . ' ' . $testfile . ' -lgtest -lpthread -o /home/jduc/gtest-1.7.0/samples/main 2>&1 1>/dev/null');
             shell_exec('/home/jduc/gtest-1.7.0/samples/main --gtest_color=yes | sh /home/jduc/gtest-1.7.0/samples/ansi2html.sh > ' . storage_path() . '/test.html');
             return View::make('compiler.compiler');
         }
