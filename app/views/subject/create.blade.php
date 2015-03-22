83747306e27116e219a956866f576ac6192e91b2
D0d0 <jozef.d13@gmail.com>
2015-03-22 13:08:12 +0100

    prepare for test

--- app/views/subject/create.blade.php
+++ app/views/subject/create.blade.php
@@ -6,48 +6,48 @@
     <div class="form-group thumbnail">
         <div class="form-group {{ $errors->has('name') ? "has-error" : "" }}">
             <div class="col-md-3">
-                {{ Form::label('name', '.meno') }}
+                {{ Form::label('name', 'Názov predmetu') }}
             </div>
             <div class="col-md-9">
-                {{ Form::text('name', '', array('class'=>'form-control', 'placeholder' => '.meno')) }}
+                {{ Form::text('name', '', array('class'=>'form-control', 'placeholder' => 'Názov predmetu')) }}
                 {{ Form::errorMsg('name') }}
             </div>
         </div>
         <div class="form-group {{ $errors->has('year') ? "has-error" : "" }}">
             <div class="col-md-3">
-                {{ Form::label('year', '.rok') }}
+                {{ Form::label('year', 'Rok') }}
             </div>
             <div class="col-md-9">
-                {{ Form::text('year', '', array('class'=>'form-control', 'placeholder' => '.rok')) }}
+                {{ Form::text('year', '', array('class'=>'form-control', 'placeholder' => 'Rok')) }}
                 {{ Form::errorMsg('year') }}
             </div>
         </div>
         <div class="form-group {{ $errors->has('session') ? "has-error" : "" }}">
             <div class="col-md-3">
-                {{ Form::label('session', '.semester') }}
+                {{ Form::label('session', 'Semester') }}
             </div>
             <div class="col-md-9">
                 {{ Form::select('session', array(
-                            Subject::WINTER => '.zimny',
-                            Subject::SUMMER => '.letny'),
+                            Subject::WINTER => 'zimný',
+                            Subject::SUMMER => 'letný'),
                     '', array('class' => 'form-control')) }}
                 {{ Form::errorMsg('session') }}
             </div>
         </div>
         <div class="form-group {{ $errors->has('teacher') ? "has-error" : "" }}">
             <div class="col-md-3">
-                {{ Form::label('teacher', '.učiteľ') }}
+                {{ Form::label('teacher', 'Učiteľ') }}
             </div>
             <div class="col-md-9">
                 <select id="teacher" name="teacher" class="form-control">
                     @foreach(User::teachers()->orderBy('surname')->get() as $user)
-                    <option value="{{ $user->id }}">{{{ $user->getSurnameName() }}}</option>
+                        <option value="{{ $user->id }}" @if($user-id == Auth::id()) selected @endif>{{{ $user->getSurnameName() }}}</option>
                     @endforeach
                 </select>
                 {{ Form::errorMsg('teacher') }}
             </div>
         </div>
-        {{Form::submit('.vytvor', array('class'=>'btn btn-default'))}}
+        {{Form::submit('Vytvor', array('class'=>'btn btn-default'))}}
     </div>
     {{ Form::close() }}
 </div>
