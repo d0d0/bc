b39b170d44b4a821be244c92fe1ab561fb7acb57
D0d0 <jozef.d13@gmail.com>
2015-03-22 21:37:41 +0100

    test result

--- app/controllers/SolutionController.php
+++ app/controllers/SolutionController.php
@@ -81,8 +81,8 @@ class SolutionController extends BaseController {
             shell_exec('timeout 5s ' . $path . '/main --gtest_color=yes --gtest_output=xml:' . $path . '/s.xml | sh /home/jduc/gtest-1.7.0/samples/ansi2html.sh > ' . $path . '/test.html');
             $result = File::get($path . '/s.xml');
             $parsed = Parser::xml($result);
-            foreach($parsed as $pars){
-                dd($pars);
+            foreach($parsed as $key => $pars){
+                dd($key);
             }
             File::deleteDirectory($path);
             return $parsed;
