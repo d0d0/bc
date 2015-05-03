fbdf6a4af139a463416bf0eb17af572c42fc67ac
D0d0 <jozef.d13@gmail.com>
2015-05-03 15:17:55 +0200

    prepare for virtual machine

--- app/controllers/SolutionController.php
+++ app/controllers/SolutionController.php
@@ -90,8 +90,9 @@ class SolutionController extends BaseController {
                 $error = str_replace($path, '', $error);
                 return '<pre style="color: red">' . $error . '</pre>';
             }
-            shell_exec('timeout 5s ' . $path . '/main.out --gtest_output=xml:' . $path . '/test/tmp/s.xml');
-            /*if (File::exists($path . '/test/tmp/s.xml')) {
+            shell_exec('timeout 5s linux rootfstype=hostfs rootflags=' . $path . '/test rw mem=48M > ' . $path . '/test/tmp/log 2>&1; echo $?>' . $path . 'test/tmp/exitcode.err');
+            //shell_exec('timeout 5s ' . $path . '/main.out --gtest_output=xml:' . $path . '/test/tmp/s.xml');
+            if (File::exists($path . '/test/tmp/s.xml')) {
                 $result = File::get($path . '/test/tmp/s.xml');
                 $parsed = Parser::xml($result);
                 $result = "";
@@ -133,7 +134,7 @@ class SolutionController extends BaseController {
             } else {
                 $result = '<pre style="color: red">Time limit. Skontroluj nekonečné while cykly alebo neefektívny algoritmus.</pre>';
             }
-            $result = str_replace($path, '', $result);*/
+            $result = str_replace($path, '', $result);
             return $result;
         }
     }
