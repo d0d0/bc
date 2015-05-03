38f4844c48ed188f1ee25b1fac926b20e4aab9ed
D0d0 <jozef.d13@gmail.com>
2015-05-03 15:28:38 +0200

    prepare for virtual machine

--- app/controllers/SolutionController.php
+++ app/controllers/SolutionController.php
@@ -90,7 +90,7 @@ class SolutionController extends BaseController {
                 $error = str_replace($path, '', $error);
                 return '<pre style="color: red">' . $error . '</pre>';
             }
-            shell_exec('timeout 20s linux rootfstype=hostfs rootflags=' . $path . '/test rw mem=48M > ' . $path . '/test/tmp/log 2>&1; echo $?>' . $path . 'test/tmp/exitcode.err');
+            shell_exec('linux rootfstype=hostfs rootflags=' . $path . '/test rw mem=48M > ' . $path . '/test/tmp/log 2>&1; echo $?>' . $path . 'test/tmp/exitcode.err');
             //shell_exec('timeout 5s ' . $path . '/main.out --gtest_output=xml:' . $path . '/test/tmp/s.xml');
             if (File::exists($path . '/test/tmp/s.xml')) {
                 $result = File::get($path . '/test/tmp/s.xml');
